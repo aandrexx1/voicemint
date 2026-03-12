2ce36509aff511ec44ac7aca45aedee60d5f9523
Andrea <andreamarchini111@gmail.com>
2026-03-12 19:31:52 +0100

    add DB connection

--- backend/models.py
+++ backend/models.py
@@ -14,7 +14,7 @@ if DATABASE_URL.startswith("postgres://"):
 if "sqlite" in DATABASE_URL:
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 else:
-    engine = create_engine(DATABASE_URL)
+    engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
